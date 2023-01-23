16f1c1c85426c2a5581464a10940ad87dd0354f8
rammktx <123376855+rammktx@users.noreply.github.com>
2023-01-23 12:23:44 -0500

    1 file modified

--- Post.tsx
+++ Post.tsx
@@ -9,6 +9,7 @@ export default function Post(props) {
       <p>{props.post.body}</p>
       <h3>Comments:</h3>
       <p> {props.post.comments[0]} </p>
+
       {/* props.post.comments.map((comment) => (
         <p> {comment} </p>
       )) */}
