0974fe02b0b4cbb92394fdfd2784a18ea97783e7
rammktx <123376855+rammktx@users.noreply.github.com>
2023-01-23 13:23:21 -0500

    1 file modified, 1 added

---
+++ Comment.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import './style.css';
+
+export default function Comment(props) {
+  return <p>{props.body}</p>;
+}
--- Post.tsx
+++ Post.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import './style.css';
 
+import Comment from './Comment';
+
 export default function Post(props) {
   return (
     <div className="center">
@@ -8,11 +10,10 @@ export default function Post(props) {
       <p>by {props.post.author}</p>
       <p>{props.post.body}</p>
       <h3>Comments:</h3>
-      <p> {props.post.comments[0]} </p>
 
-      {/* props.post.comments.map((comment) => (
-        <p> {comment} </p>
-      )) */}
+      {props.post.comments.map((comment) => (
+        <Comment body={comment} />
+      ))}
     </div>
   );
 }
